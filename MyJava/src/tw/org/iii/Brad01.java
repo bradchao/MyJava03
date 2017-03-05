a05b9a9cccbd98885739418c57571e57a08044e5
Brad Chao <brad@brad.tw>
2017-03-05 13:20:01 +0800

    2017030501

---
+++ MyJava/src/tw/org/iii/Brad01.java
@@ -0,0 +1,30 @@
+package tw.org.iii;
+
+public class Brad01 {
+	/*
+	 * 這素偶的第一支城市
+	 * ㄇbghhhhj
+	 */
+	
+	/**
+	 * 
+	 * @param brad
+	 */
+	
+	public static void main(String[] brad) {
+		// TODO Auto-generated method stub
+		
+		// abcdefg
+		
+		// XXX ????
+	}
+	
+	void m1(){
+		// TODO 粉重要
+	}
+	
+	void m2(){
+		// FIXME 有蟲
+	}
+
+}
