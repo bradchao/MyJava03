9e8950b9053bc8d14e912d39bfe6e7ef05b55920
Brad Chao <brad@brad.tw>
2017-03-12 17:02:56 +0800

    v6

---
+++ MyJava/src/tw/org/iii/Brad19.java
@@ -0,0 +1,16 @@
+package tw.org.iii;
+
+public class Brad19 {
+
+	public static void main(String[] args) {
+		Bike b1 = new Bike();
+		Bike b2 = new Bike();
+		System.out.println(b1.getSpeed());
+		b1.upSpeed();
+		b1.upSpeed();
+		b1.upSpeed();
+		b1.upSpeed();
+		System.out.println(b1.getSpeed());
+	}
+
+}
