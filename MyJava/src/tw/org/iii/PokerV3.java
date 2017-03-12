b5567fad165a7ce1e829fee0619903b5a2dede6b
Brad Chao <brad@brad.tw>
2017-03-12 11:13:59 +0800

    2017031202

--- MyJava/src/tw/org/iii/PokerV3.java
+++ MyJava/src/tw/org/iii/PokerV3.java
@@ -3,13 +3,13 @@ package tw.org.iii;
 public class PokerV3 {
 
 	public static void main(String[] args) {
-		long start = System.currentTimeMillis();
-		boolean[] check = new boolean[10];	// 0 - 51: false
-		int[] poker = new int[10]; // 0- 51: 0
+		// 洗牌
+		boolean[] check = new boolean[52];	// 0 - 51: false
+		int[] poker = new int[52]; // 0- 51: 0
 		int temp; int counter=0;
-		for (int i=0; i<10; i++){
+		for (int i=0; i<52; i++){
 			do{
-				temp = (int)(Math.random()*10);
+				temp = (int)(Math.random()*52);
 				counter++;
 			}while (check[temp]);
 			
@@ -17,8 +17,24 @@ public class PokerV3 {
 			check[temp] = true;
 			System.out.println(temp);
 		}
-		System.out.println("耗時:" + (System.currentTimeMillis() - start));
-		System.out.println("次數:" + counter);
+		// 發牌 => 四個玩家 => 每家有13張
+		int[][] player = new int[4][13];
+		for (int i=0; i<poker.length; i++){
+			player[i%4][i/4] = poker[i];
+		}
+		System.out.println("--");
+		// 攤牌
+		for (int[] cards : player){
+			for (int card : cards){
+				System.out.print(card + "  ");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+		
 	}
 
 }
